8e834afac529ffb2e9a07ceeffdf9f89dd8c69f6
Achilles <achillespentest@gmail.com>
2025-07-17 21:20:04 +0530

    banner.py

---
+++ voidrecon/core/banner.py
@@ -0,0 +1,17 @@
+
+# voidrecon/core/banner.py
+
+def print_banner():
+    banner = r"""
+    888     888          d8b      888 8888888b.                                    
+    888     888          Y8P      888 888   Y88b                                   
+    888     888                   888 888    888                                   
+    Y88b   d88P  .d88b.  888  .d88888 888   d88P .d88b.   .d8888b .d88b.  88888b.  
+     Y88b d88P  d88""88b 888 d88" 888 8888888P" d8P  Y8b d88P"   d88""88b 888 "88b 
+      Y88o88P   888  888 888 888  888 888 T88b  88888888 888     888  888 888  888 
+       Y888P    Y88..88P 888 Y88b 888 888  T88b Y8b.     Y88b.   Y88..88P 888  888 
+        Y8P      "Y88P"  888  "Y88888 888   T88b "Y8888   "Y8888P "Y88P"  888  888 
+
+                    [*] VoidRecon - Modular Recon Toolkit
+    """
+    print(banner)
--- voidrecon/core/framework.py
+++ voidrecon/core/framework.py
@@ -7,23 +7,7 @@ import os
 import importlib
 import voidrecon.core.module as module
 import voidrecon.modules as modules
-
-class banner:
-    def print_banner():
-        banner = r"""
-     888     888          d8b      888 8888888b.                                    
-     888     888          Y8P      888 888   Y88b                                   
-     888     888                   888 888    888                                   
-     Y88b   d88P  .d88b.  888  .d88888 888   d88P .d88b.   .d8888b .d88b.  88888b.  
-      Y88b d88P  d88""88b 888 d88" 888 8888888P" d8P  Y8b d88P"   d88""88b 888 "88b 
-       Y88o88P   888  888 888 888  888 888 T88b  88888888 888     888  888 888  888 
-        Y888P    Y88..88P 888 Y88b 888 888  T88b Y8b.     Y88b.   Y88..88P 888  888 
-         Y8P      "Y88P"  888  "Y88888 888   T88b "Y8888   "Y8888P "Y88P"  888  888 
-
-                        [*] VoidRecon - Modular Recon Toolkit
-        """
-        print(banner)
-
+import voidrecon.core.banner as banner
 
 class VoidReconShell(cmd.Cmd):
     prompt = "# VoidRecon > "
